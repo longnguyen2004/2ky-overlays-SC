81b61a674cc04dd4de1a617c637af3c71e9301ba
Long Nguyen <nguyen.long.908132@gmail.com>
2023-08-12 17:42:43 +0700

    Add Nx and build:all script

---
+++ scripts/copy-output.js
@@ -0,0 +1,16 @@
+import cpy from "cpy";
+import { globby } from "globby";
+import pMap from "p-map";
+import { fileURLToPath } from "node:url";
+
+const rootDir = new URL("..", import.meta.url)
+
+const folders = await globby("overlays/*/dist", {
+    cwd: rootDir,
+    onlyDirectories: true
+});
+await pMap(folders, (folder) => {
+    const overlayName = folder.replaceAll(/overlays\/|\/dist/g, "");
+    console.log(`Copying ${overlayName} => dist/${overlayName}`);
+    return cpy(folder + "/*", `dist/${overlayName}`, { cwd: fileURLToPath(rootDir) })
+}, { concurrency: 3 });
