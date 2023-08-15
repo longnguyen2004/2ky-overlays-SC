4b5d66d06d350241f5d4978a041c9287912b1604
Long Nguyen <nguyen.long.908132@gmail.com>
2023-08-15 16:49:58 +0700

    Allow for in game leaderboard fetching

--- overlays/mania_simple_leaderboard/src/App.svelte
+++ overlays/mania_simple_leaderboard/src/App.svelte
@@ -4,38 +4,43 @@
     GameStates,
     type Tokens,
   } from "osu-stream-companion-store";
-  import { config } from "./lib/config";
   import * as Client from "./lib/api";
+  import { config } from "./lib/config";
+  import { parseInGameLeaderboard } from "./lib/ingameLeaderboard";
 
   import Leaderboard from "./components/Leaderboard.svelte";
 
   let fetcher: Client.ApiClient;
-  const server = config.servers.find(({ name }) => name === config.current);
-  if (!server) {
-    console.error(
-      "Selected server is not in the server list!\n" +
-        `Selected server: ${config.current}\n` +
-        `Server list: ${config.servers.map((el) => el.name).join(", ")}`
-    );
+  if (config.current === "ingame") {
+    fetcher = new Client.Null();
   } else {
-    switch (server.type) {
-      case "osu_v1": {
-        fetcher = new Client.OsuV1(server.endpoint, server.api_key);
-        break;
-      }
-      case "osu_v2": {
-        const { endpoint, auth_endpoint, client_id, client_secret } = server;
-        fetcher = new Client.OsuV2(
-          endpoint,
-          auth_endpoint,
-          client_id,
-          client_secret
-        );
-        break;
-      }
-      case "akatsuki_v1": {
-        fetcher = new Client.AkatsukiV1(server.endpoint);
-        break;
+    const server = config.servers.find(({ name }) => name === config.current);
+    if (!server) {
+      console.error(
+        "Selected server is not in the server list!\n" +
+          `Selected server: ${config.current}\n` +
+          `Server list: ${config.servers.map((el) => el.name).join(", ")}`
+      );
+    } else {
+      switch (server.type) {
+        case "osu_v1": {
+          fetcher = new Client.OsuV1(server.endpoint, server.api_key);
+          break;
+        }
+        case "osu_v2": {
+          const { endpoint, auth_endpoint, client_id, client_secret } = server;
+          fetcher = new Client.OsuV2(
+            endpoint,
+            auth_endpoint,
+            client_id,
+            client_secret
+          );
+          break;
+        }
+        case "akatsuki_v1": {
+          fetcher = new Client.AkatsukiV1(server.endpoint);
+          break;
+        }
       }
     }
   }
@@ -48,11 +53,17 @@
     "mapid",
     "gameMode",
     "status",
+    "songSelectionScores"
   ] as const satisfies readonly Tokens[];
   const store = companion("localhost:20727", tokens);
 
+  let inGameLeaderboard: Client.ScoreEntry[] = [];
+
   $: ({ values } = $store);
-  $: ({ gameMode, mapid } = values);
+  $: ({ gameMode, mapid, songSelectionScores } = values);
+  $: if (songSelectionScores) {
+    inGameLeaderboard = parseInGameLeaderboard(songSelectionScores)
+  }
   $: current = {
     username: values.username || "",
     accuracy: values.acc || 0,
@@ -82,9 +93,28 @@
 >
   {#if isGameplay && gameMode !== undefined}
     {#await fetcher.getMapLeaderboard(mapid || -1, gameMode) then res}
-      {@const { scores: leaderboard, limit } = res}
-      {@const scores = [...(leaderboard || []), current]}
-      <Leaderboard {scores} {limit} {cardWidth} {cardHeight} {cardCount} {cardGap} />
+      {#if res.scores}
+        {@const { scores: leaderboard, limit } = res}
+        {@const scores = [...(leaderboard || []), current]}
+        <Leaderboard
+          {scores}
+          {limit}
+          {cardWidth}
+          {cardHeight}
+          {cardCount}
+          {cardGap}
+        />
+      {:else}
+        {@const scores = inGameLeaderboard}
+        <Leaderboard
+          scores={[...scores, current]}
+          limit={scores.length}
+          {cardWidth}
+          {cardHeight}
+          {cardCount}
+          {cardGap}
+        />
+      {/if}
     {/await}
   {/if}
 </main>
