1b405e6b486dddf8834d2d22e563e6ea285fe016
Long Nguyen <nguyen.long.908132@gmail.com>
2023-08-15 16:21:22 +0700

    Add in game leaderboard parser

---
+++ overlays/mania_simple_leaderboard/src/lib/ingameLeaderboard.ts
@@ -0,0 +1,33 @@
+import objectHash from "object-hash";
+import type { ScoreEntry } from "./api";
+import type { GameMode, Grade } from "osu-stream-companion-store";
+
+type InGameEntry = {
+    Username: string,
+    ModsEnum: number,
+    Mode: GameMode,
+    MaxCombo: number,
+    Score: number,
+    Hit300: number,
+    Hit100: number,
+    Hit50: number,
+    HitGeki: number,
+    HitKatu: number,
+    HitMiss: number,
+    Date: string,
+    UserId: number,
+    Accuracy: number,
+    Mods: string,
+    Grade: Grade
+};
+
+export function parseInGameLeaderboard(str: string): ScoreEntry[] {
+    const leaderboard = JSON.parse(str) as InGameEntry[];
+    return leaderboard
+        .map(({ Username, Score, MaxCombo, Date }) => ({
+            username: Username,
+            score: Score,
+            max_combo: MaxCombo,
+            timestamp: Date
+        }));
+}
